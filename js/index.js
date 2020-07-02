3e22854eafcda2f7197c9e6ef34164f6101c125b
mtsiup <maria.tsiupa@gmail.com>
2020-07-02 12:45:23 +0000

    sample

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const display=document.querySelector('.display');
+    document.querySelectorAll('.digits button')
+    .forEach(button => button.addEventListener('click', digitPressed));
+
+
+
+// const display=document.querySelector('.display-input');
+// document.querySelectorAll('.digits button')
+//     .forEach(button => button.addEventListener('click', digitPressed));
+
+function digitPressed(ev) {
+    display.value += ev.target.innerText
+}
+
+
+document.querySelectorAll('.opers button')
+    .forEach(button => button.addEventListener('click', opersPressed));
+
+function opersPressed(ev) {
+    display.value += ev.target.innerText
+}
+
+document.querySelector('.equals').addEventListener('click', calculate);
+function calculate(){
+    display.value = eval(display.value);
+}
+
+document.querySelector('.clear').addEventListener('click', clean);
+function clean(){
+    display.value = "";
+}
+
+document.querySelector('.decimal').addEventListener('click', decimal);
+function decimal(){
+    display.value = ".";
+}
+
+
